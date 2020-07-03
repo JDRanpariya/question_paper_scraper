5fbcf985dc86f4a52a4c6f2090701b0597a462ec
Roshan Saravanan <roshan.saravanan001@gmail.com>
2020-07-03 14:00:27 +0530

    Added comments

--- qPull_fully_automated_beta.py
+++ qPull_fully_automated_beta.py
@@ -4,6 +4,7 @@ from time import sleep
 
 
 def generate_button_ids():
+	"""Generates the id's for all the buttons on the question paper for navigating"""
 	base_id = "ContentPlaceHolder1_rptquestioninfo_lnkbtnquestionnumber_"
 	button_ids = [ base_id+str(i) for i in range(1, 10) ]
 
@@ -12,21 +13,26 @@ def generate_button_ids():
 
 def navigate_and_return_next_question(button_id, previous_question):
 	#previous question should be unformatted 
+
+	#finding the button with id and clicking it
 	element = driver.find_element_by_id(button_id)
 	element.click()
 	
+	#looping until it's sure that the webpage loaded to the next questions
+	#and scraping the question once it's sure that the page is changed to next question
 	while True:
 		sleep(3)
 		content = driver.page_source
 		soup = BeautifulSoup(content, features="lxml")
 		next_question = soup.findAll("table", id="ContentPlaceHolder1_dlquestions")	
 
-		if next_question[0].text != previous_question[0].text:
+		if next_question[0].text == previous_question[0].text:
 			break
 	return next_question
 
 
 def add_to_file(question_text):	
+	"""Writes the questions to a text file with a clean format"""
 	#Strip newlines for the scraped question 
 	Qa = question_text.replace("\n\n\n", "")
 	Qa = Qa.replace("\n\n", "\n")
@@ -35,8 +41,10 @@ def add_to_file(question_text):
 	question_list.write("\n\n\n" + Qa)
 
 
-driver = webdriver.Chrome(""" YOUR CHROME DRIVER LOCATION """)
+driver = webdriver.Chrome("/home/roshan/Downloads/chromedriver")
 driver.get("https://engineering.jainuniversity.ac.in/")
+# driver.get("file:///home/roshan/Documents/MCQ-ANV.html")
+
 question_list = open("question_list.txt", "a")
 
 input("\n\nHit enter to start scraping....")
